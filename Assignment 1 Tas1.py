b4d0d2f7b263f568038abcf23009d312001e0211
sunamorpittu <sunamorpittu@gmail.com>
2025-06-30 22:25:00 +0530

    Add files via upload

---
+++ Assignment 1 Tas1.py
@@ -0,0 +1,12 @@
+#Task 1: Perform Basic Mathematical Operations
+
+x=int(input('Enter the First number: '))
+y=int(input('Enter the Second Number: '))
+z=x+y
+print('Addition: ',z)
+s=x-y
+print('Substraction: ',s)
+m=x*y
+print('Multiplication: ',m)
+d=x/y
+print('Division: ',d)
---
+++ Assignment 1 Task 2.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+
+x=input('Enter Your First Name: ')
+y=input('Enter Your Last Name: ')
+full_name= x +' '+y
+print(f'Hello {full_name}! Welcome to the Python Program.')
